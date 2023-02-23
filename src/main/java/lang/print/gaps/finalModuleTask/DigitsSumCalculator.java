53a3c2b365be392255f6cac606fd389277dbda2f
shandyz <axshandyz@gmail.com>
2023-02-23 20:08:33 +0100

    work work work

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int num = number;
+        int sum = 0;
+        while (num > 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        long reversedNum = 0;
+        long input_long = number;
+
+        while (input_long != 0) {
+            reversedNum = reversedNum * 10 + input_long % 10;
+            input_long = input_long / 10;
+        }
+        System.out.println(reversedNum);
     }
+
 }
